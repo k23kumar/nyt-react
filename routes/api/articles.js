8279ca2d6a5c8880bb1dff0f12960e90704c32bb
k23kumar <k23kumar@gmail.com>
2018-10-11 20:54:22 -0700

    edit controller

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const articlesController = require("../../controllers/articlesController");
+const articlesController = require("../../controllers/articlescontroller");
 
 // Matches with "/api/articles"
 router.route("/")
